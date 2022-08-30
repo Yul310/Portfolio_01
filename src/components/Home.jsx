9e55892a79b6ffbd8dddbcf7ea43f7c44c00a87d
Yul310 <hsy3102@gmail.com>
2022-08-29 22:13:11 -0400

    css updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-scroll'
 
 const Home = () => {
     return (
-        <div name='home' className='w-full h-screen bg-[#0a192f] '>
+        <div name='home' className='w-full h-screen bg-[#343434] '>
 
             {/* Container */}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
-                <p className='text-yellow-300 text-2xl'>Hi, my name is </p>
+                <p className='text-[#F8FF24] text-2xl'>Hi, my name is </p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-white'>Samyul Huh.</h1>
-                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a frontend Developer</h2>
-                <p className=' text-[#8892b0] py-4 max-w-700 text-md'> who has an architecture background and loves problem solving and building, structuring digital experiences. </p>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#909090]'>I'm a frontend Developer</h2>
+                <p className=' text-[#909090] py-4 max-w-700 text-md'> who has an architecture background and loves problem solving and building, structuring digital experiences. </p>
 
 
                 <div>
-                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center rounded-lg hover:bg-yellow-300 hover:border-yellow-300 hover:text-[#0a192f]'>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center rounded-lg hover:bg-[#F8FF24] hover:border-[#F8FF24] hover:text-[#0a192f]'>
 
                         <Link to="work" spy={true} smooth={true} duration={500} className='group-hover:font-black' >
                         View Work
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#343434] text-gray-300">
       <div>
         {/* <img src={Logo} alt="Logo Image" style={{ width: '50px' }} /> */}
       </div>
@@ -99,22 +99,22 @@ const Navbar = () => {
       {/* <div className='hidden lg:flex fixed flex-col top-[35%] left-0'> */}
       <div className='flex fixed  top-[0%] left-0'>
         <ul className='flex flex-row'>
-          <li className='w-[60px] h-[100px] flex justify-between items-center mt-[-40px] hover:mt-[-5px] duration-300 bg-yellow-300'>
-            <a className='flex flex-col justify-between items-center  text-center w-full text-[#0a192f]'
+          <li className='w-[60px] h-[100px] flex justify-between items-center mt-[-40px] hover:mt-[-5px] duration-300 bg-[#343434]'>
+            <a className='flex flex-col justify-between items-center  text-center w-full text-[#F8FF24]'
               href="https://www.linkedin.com/in/samyul-huh/">
               Linkedin
               {"\n"}
-              <p className='text-yellow-300'>-</p>
+              <p className=' text-[#343434]'>-</p>
               {"\n"}
               <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[60px] h-[100px] flex justify-between items-center mt-[-40px] hover:mt-[-5px] duration-300 bg-yellow-300'>
-            <a className='flex flex-col justify-between items-center  text-center w-full text-[#0a192f]'
+          <li className='w-[60px] h-[100px] flex justify-between items-center mt-[-40px] hover:mt-[-5px] duration-300 bg-[#343434]'>
+            <a className='flex flex-col justify-between items-center  text-center w-full text-[#F8FF24]'
               href="https://github.com/Yul310">
               Github
               {"\n"}
-              <p className='text-yellow-300'>-</p>
+              <p className=' text-[#343434]'>-</p>
               {"\n"}
               <FaGithub size={30} />
             </a>
@@ -129,12 +129,12 @@ const Navbar = () => {
               <HiOutlineMail size={30} />
             </a>
           </li> */}
-          <li className='w-[60px] h-[100px] flex justify-between items-center mt-[-40px] hover:mt-[-5px] duration-300 bg-yellow-300'>
-            <a className='flex flex-col justify-between items-center  text-center w-full text-[#0a192f]'
+          <li className='w-[60px] h-[100px] flex justify-between items-center mt-[-40px] hover:mt-[-5px] duration-300 bg-[#343434]'>
+            <a className='flex flex-col justify-between items-center  text-center w-full text-[#F8FF24]'
               href="https://drive.google.com/file/d/1bvbIlW5vcdgQRvG3kyAFHlxnfjWCh7o0/view?usp=sharing">
               Resume
               {"\n"}
-              <p className='text-yellow-300'>-</p>
+              <p className=' text-[#343434]'>-</p>
               {"\n"}
               <BsFillPersonLinesFill size={30} />
             </a>
